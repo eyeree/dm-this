7d5517246af685092e7f24b9851c7135cff10f2f
mike@eyeree.me <mike@eyeree.me>
2025-03-29 15:21:00 -0700

    using qdrant

--- src/util/rule-set/colqwen2.py
+++ src/util/rule-set/colqwen2.py
@@ -3,101 +3,212 @@ from tqdm import tqdm
 from colpali_engine.models import ColQwen2, ColQwen2Processor
 from pdf2image import convert_from_path
 from pypdf import PdfReader
-from pathlib import PosixPath
+from pathlib import Path, PosixPath
 from PIL import Image
 import os
+from qdrant_client import QdrantClient
+from qdrant_client.http import models
+import stamina
+from rich import print as r_print
 
-model_name = "vidore/colqwen2-v0.1"
+THREAD_COUNT = 16
+BATCH_SIZE = 6
 
-model = ColQwen2.from_pretrained(
-    model_name, 
-    torch_dtype=torch.bfloat16, 
-    device_map="auto",
-    local_files_only=True
-)
-processor = ColQwen2Processor.from_pretrained(
-    model_name, 
-    use_fast=True
-)
-model = model.eval()
+class RuleSetRag:
 
-def process_pdf(pdf_path:PosixPath):
-    print(f'Processing {pdf_path}...')
+    _vector_size = 128
+    _page_images_collection_name = 'rules_SRD3_5'
+    _model_name = "vidore/colqwen2-v0.1"
+    _page_image_path_cache:list[str] = []
 
-    page_image_path = pdf_path.parent.joinpath('page_image')
-    page_image_path.mkdir(exist_ok=True)
-    # save_pdf_page_text(pdf_path)
+    def __init__(self, rule_set_path:Path):
+        self._rule_set_path = rule_set_path
+        self._page_image_path = self._rule_set_path.joinpath('page_image')
+        self._page_image_path.mkdir(exist_ok=True)
+        self._qdrant_path = self._rule_set_path.joinpath('qdrant')
+        self._qdrant_path.mkdir(exist_ok=True)
+        self._qdrant_client = QdrantClient(path=str(self._qdrant_path))
 
-    page_text_path = pdf_path.parent.joinpath('page_text')
-    page_text_path.mkdir(exist_ok=True)
-    # save_pdf_page_images(pdf_path)
-
-    generate_image_embeddings(page_image_path)
-
-def save_pdf_page_images(pdf_path, page_image_path):
-    
-    convert_from_path(
-        pdf_path, 
-        fmt='png',
-        output_file=pdf_path.stem, 
-        output_folder=page_image_path,
-        paths_only=True,
-        thread_count=16
-    )
-
-def save_pdf_page_text(pdf_path, page_text_path):
-
-    reader = PdfReader(pdf_path)
-
-    for page_number in range(len(reader.pages)):
-        page = reader.pages[page_number]
-        text = page.extract_text()
-        with open(page_text_path.joinpath(f'{pdf_path.stem}-{page_number+1:04}.txt'), mode='w') as file:
-            file.write(text)
-
-def generate_image_embeddings(page_image_path):
-
-    page_embeddings = []
-    
-    # Get all image files from the directory
-    image_files = sorted([
-        os.path.join(page_image_path, f) 
-        for f in os.listdir(page_image_path) 
-        if f.lower().endswith(('.png', '.jpg', '.jpeg'))
-    ])
-    
-    # Process images in batches
-    batch_size = 2
-    for i in tqdm(range(0, len(image_files), batch_size)):
-        print('processing', i)
-        # Load only the current batch of images
-        batch_images = []
-        for j in range(i, min(i + batch_size, len(image_files))):
-            try:
-                img = Image.open(image_files[j])
-                batch_images.append(img)
-            except Exception as e:
-                print(f"Error loading image {image_files[j]}: {e}")
-                continue
+        self._model = ColQwen2.from_pretrained(
+            self._model_name, 
+            torch_dtype=torch.bfloat16, 
+            device_map="auto",
+            local_files_only=True
+        )
+        self._processor = ColQwen2Processor.from_pretrained(
+            self._model_name, 
+            use_fast=True
+        )
+        self._model = self._model.eval()
         
-        if not batch_images:
-            continue
+        self._load_page_image_path_cache()
+
+    def create_index(self):
+        self._create_page_images()
+        self._create_page_images_collection()
+        self._index_page_image_embeddings()
+
+    def _create_page_images(self):
+
+        print(f'Deleting page images in {self._page_image_path}...')
+        for image_file_path in self._page_image_path.iterdir():
+            image_file_path.unlink()
+
+        for pdf_file_path in self._rule_set_path.glob('*.pdf'):
+            print(f'Creating page images for {pdf_file_path} in {self._page_image_path}...')
+            self._create_page_images_for_pdf(pdf_file_path)
+
+        self._load_page_image_path_cache()
+
+    def _create_page_images_for_pdf(self, pdf_file_path:Path):
+        convert_from_path(
+            pdf_file_path, 
+            fmt='png',
+            output_file=pdf_file_path.stem, 
+            output_folder=self.page_image_path,
+            paths_only=True,
+            thread_count=THREAD_COUNT
+        )
+
+    def _create_page_images_collection(self):
+
+        if self._qdrant_client.collection_exists(self._page_images_collection_name):
+            self._qdrant_client.delete_collection(self._page_images_collection_name)
+
+        self._qdrant_client.create_collection(
+            collection_name=self._page_images_collection_name,  # the name of the collection
+            on_disk_payload=True,  # store the payload on disk
+            optimizers_config=models.OptimizersConfigDiff(
+                indexing_threshold=0
+            ),  # it can be useful to switch this off when doing a bulk upload and then manually trigger the indexing once the upload is done
+            vectors_config=models.VectorParams(
+                size=self.vector_size,
+                distance=models.Distance.COSINE,
+                multivector_config=models.MultiVectorConfig(
+                    comparator=models.MultiVectorComparator.MAX_SIM
+                ),
+                quantization_config=models.ScalarQuantization(
+                    scalar=models.ScalarQuantizationConfig(
+                        type=models.ScalarType.INT8,
+                        quantile=0.99,
+                        always_ram=True,
+                    ),
+                ),
+            ),
+        )
+
+    def _index_page_image_embeddings(self):
+
+        for (i, image_file_paths, embeddings) in self._generate_image_embeddings(self._page_image_path_cache):
+            self._upsert_to_qdrant(self._page_images_collection_name, i, image_file_paths, embeddings)
+
+        self._qdrant_client.update_collection(
+            collection_name=self._page_images_collection_name,
+            optimizer_config=models.OptimizersConfigDiff(indexing_threshold=10),
+        )
+
+    def _generate_image_embeddings(self, image_file_paths:list[str]):
+
+        for i in tqdm(range(0, len(image_file_paths), BATCH_SIZE), "Generating embeddings"):
+            batch_images = []
+            for j in range(i, min(i + BATCH_SIZE, len(image_file_paths))):
+                try:
+                    img = Image.open(image_file_paths[j])
+                    batch_images.append(img)
+                except Exception as e:
+                    print(f"Error loading image {image_file_paths[j]}: {e}")
+                    continue
             
-        # Process the batch
-        batch_doc = processor.process_images(batch_images)
-        
-        # Generate embeddings
-        with torch.no_grad():
-            batch_doc = {k: v.to(model.device) for k, v in batch_doc.items()}
-            embeddings_doc = model(**batch_doc)
-            embeddings = list(torch.unbind(embeddings_doc.to("cpu")))
-            print('  ->', embeddings)
-            page_embeddings.extend(embeddings)
-        
-        # Explicitly close images to free memory
-        for img in batch_images:
-            img.close()
-    
-    return page_embeddings
+            if not batch_images:
+                continue
+                
+            batch_doc = self._processor.process_images(batch_images)
+            
+            # Generate embeddings
+            with torch.no_grad():
+                batch_doc = {k: v.to(self._model.device) for k, v in batch_doc.items()}
+                embeddings_doc = self._model(**batch_doc)
+                embeddings = list(torch.unbind(embeddings_doc.to("cpu")))
+                yield (i, image_file_paths, embeddings)
+            
+            # Explicitly close images to free memory
+            for img in batch_images:
+                img.close()
 
-process_pdf(PosixPath('./content/rules/SRD3_5/SRD_3.5_Complete_2004_ver_1.0.pdf'))
+    @stamina.retry(on=Exception, attempts=3)
+    def _upsert_to_qdrant(self, collection_name, i, image_file_paths, image_embeddings):
+
+        points = []
+        for j, embedding in enumerate(image_embeddings):
+            # Convert the embedding to a list of vectors
+            multivector = embedding.cpu().float().numpy().tolist()
+            points.append(
+                models.PointStruct(
+                    id=i + j,  # we just use the index as the ID
+                    vector=multivector,  # This is now a list of vectors
+                    payload={
+                        "source": image_file_paths[i + j]
+                    },  # can also add other metadata/data
+                )
+            )
+
+        try:
+            self._qdrant_client.upsert(
+                collection_name=collection_name,
+                points=points,
+                wait=False,
+            )
+        except Exception as e:
+            print(f"Error during upsert: {e}")
+            return False
+        return True
+
+    def show_embedding_dimensions(self, image_path:str):
+
+        sample_image = Image.open(image_path)
+
+        with torch.no_grad():
+            sample_batch = self._processor.process_images([sample_image]).to(
+                self._model.device
+            )
+            sample_embedding = self._model(**sample_batch)
+            print('shape:', sample_embedding.shape)
+            print('size:', sample_embedding.shape[2])
+
+    def _load_page_image_path_cache(self):
+        self._page_image_path_cache = sorted([
+            str(image_file_path) for image_file_path in self._page_image_path.iterdir()
+        ])
+
+    def get_page_image_paths(self, query_text:str, top_k:int = 5) -> list[str]:
+
+        with torch.no_grad():
+            batch_query = self._processor.process_queries([query_text + ' ' + query_text.capitalize() + ' ' +  query_text.lower() + ' ' +  query_text.upper()]).to(
+                self._model.device
+            )
+            query_embedding = self._model(**batch_query)
+
+        multivector_query = query_embedding[0].cpu().float().numpy().tolist()
+
+        query_result = self._qdrant_client.query_points(
+            collection_name=self._page_images_collection_name, query=multivector_query, limit=top_k
+        )
+
+        row_ids = [r.id for r in query_result.points]
+        return [self._page_image_path_cache[row_id] for row_id in row_ids]
+
+
+rule_set_rag = RuleSetRag(PosixPath('./content/rules/SRD3_5'))
+
+while True:
+    try:
+        query_text = input("query: ")
+        if query_text == "":
+            break
+        matching_image_file_paths = rule_set_rag.get_page_image_paths(query_text, 10)
+        print('\n'.join(['  ' + path for path in matching_image_file_paths]))
+    except EOFError:
+        break
+    except KeyboardInterrupt:
+        break
+print('\n\n')
